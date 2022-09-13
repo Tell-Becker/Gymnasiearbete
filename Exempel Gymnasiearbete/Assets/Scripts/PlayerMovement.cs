0e26c1cf7cc4d1370c559e7139bbc10668f6e4ec
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2022-09-13 13:02:52 +0200

    Can no longer dubblejump
    ;)

--- Exempel Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
+++ Exempel Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
 
 
     [SerializeField] private float speed;
+    [SerializeField] private float raycastDownDistance = 0.4f;
+    [SerializeField] private LayerMask whatIsGround;
     private Rigidbody2D body;
  
     private void Awake()
@@ -23,6 +25,14 @@ public class PlayerMovement : MonoBehaviour
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
  
         if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDownDistance, whatIsGround);
+            if (hit.collider != null) 
+            {
+                body.velocity = new Vector2(body.velocity.x, speed);
+            }
+            
+        }
+            
     }
 }
