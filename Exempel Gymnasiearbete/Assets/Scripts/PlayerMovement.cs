a35ac109121a9bff8c604978a70fa9b9784a393a
Tell-Becker <lwtb04@gmail.com>
2022-10-04 17:50:28 +0200

    still trying to fix grapplng hook.
    Didn't work i really need to know how to check if the mouse clcked on a cerntain object (layer)

--- Exempel Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
+++ Exempel Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform bottomPoint;
     private int jumpsLeft;
     private Rigidbody2D body;
+    private float timer = 0;
  
     private void Awake()
     {
@@ -19,9 +20,15 @@ public class PlayerMovement : MonoBehaviour
  
     private void Update()
     {
-        while (Input.GetMouseButtonDown(0) == false)
+        //Detta leder till att man glidar efter man tryckt a eller d :/
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        }
+        // Detta ar inte en fungerande losning, nar man anvander hooken glider man fortfarnade och det ser inte bra ut nar man bara stannar
+           if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * timer, body.velocity.y);
         }
 
         if (jumpsLeft > 0 && Input.GetKeyDown(KeyCode.Space)) // kollar om det finns hopp kvar
--- Exempel Gymnasiearbete/Assets/Scripts/grappler.cs
+++ Exempel Gymnasiearbete/Assets/Scripts/grappler.cs
@@ -4,77 +4,21 @@ using UnityEngine;
 
 public class grappler : MonoBehaviour
 {
-
-    private Vector3 _mousePos;
-    private Camera _camera;
-
-    private bool _check;
-
-    private DistanceJoint2D _distanceJoint;
-
-    private LineRenderer _lineRenderer;
-
-    private Vector3 _tempPos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _camera = Camera.main;
-        _distanceJoint = GetComponent<DistanceJoint2D>();
-        _lineRenderer = GetComponent<LineRenderer>();
-        _distanceJoint.enabled = false;
-        _check = true;
-        _lineRenderer.positionCount = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        GetMousePos();
-        if (Input.GetMouseButtonDown(0) && _check)
-        {
-            _distanceJoint.enabled = true;
-            _distanceJoint.connectedAnchor = _mousePos;
-            _lineRenderer.positionCount = 2;
-            _tempPos = _mousePos;
-            _check = false;
-        }else if (Input.GetMouseButtonDown(0))
-        {
-            _distanceJoint.enabled = false;
-            _check = true;
-            _lineRenderer.positionCount = 0;
-        }
-        DrawLine();
-    }
-
-    private void DrawLine()
-    {
-        if(_lineRenderer.positionCount <=0) return;
-        _lineRenderer.SetPosition(0,transform.position);
-        _lineRenderer.SetPosition(1,_tempPos);
-    }
-
-    private void GetMousePos()
-    {
-        _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
-    }
-}
-
-/*
+    private Vector3 mousePos;
     public Camera mainCamera;
     public LineRenderer lineRenderer;
     public DistanceJoint2D distanceJoint;
+    public LayerMask grappleMask;
 
-    // Start is called before the first frame update
     void Start()
     {
         distanceJoint.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        //RaycastHit2D hit2D = Physics2D.Raycast(mainCamera.transform.position, mousePos, Mathf.Infinity, grappleMask);
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //&& hit2D ?
         {
             Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, mousePos);
@@ -93,4 +37,62 @@ public class grappler : MonoBehaviour
             lineRenderer.SetPosition(1, transform.position);
         }
         
-    }*/
+    }
+
+    // private Vector3 _mousePos;
+    // private Camera _camera;
+
+    // private bool _check;
+
+    // private DistanceJoint2D _distanceJoint;
+
+    // private LineRenderer _lineRenderer;
+
+    // private Vector3 _tempPos;
+
+    // public LayerMask grappleMask;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     _camera = Camera.main;
+    //     _distanceJoint = GetComponent<DistanceJoint2D>();
+    //     _lineRenderer = GetComponent<LineRenderer>();
+    //     _distanceJoint.enabled = false;
+    //     _check = true;
+    //     _lineRenderer.positionCount = 0;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     GetMousePos();
+
+    //     RaycastHit2D hit2D = Physics2D.Raycast(_camera.transform.position, _mousePos, Mathf.Infinity, grappleMask);
+    //     if (Input.GetMouseButtonDown(0) && _check) // && hit2D
+    //     {
+    //         _distanceJoint.enabled = true;
+    //         _distanceJoint.connectedAnchor = _mousePos;
+    //         _lineRenderer.positionCount = 2;
+    //         _tempPos = _mousePos;
+    //         _check = false;
+    //     }else if (Input.GetMouseButtonDown(0))
+    //     {
+    //         _distanceJoint.enabled = false;
+    //         _check = true;
+    //         _lineRenderer.positionCount = 0;
+    //     }
+    //     DrawLine();
+    // }
+
+    // private void DrawLine()
+    // {
+    //     if(_lineRenderer.positionCount <=0) return;
+    //     _lineRenderer.SetPosition(0,transform.position);
+    //     _lineRenderer.SetPosition(1,_tempPos);
+    // }
+
+    // private void GetMousePos()
+    // {
+    //     _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+    // }
+}
